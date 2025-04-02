fdf00bd0b5f2b275a46e8c0914ffb359af9e29d7
Andoni Berho <aberho001@iutbayonne.univ-pau.fr>
2025-04-02 15:35:28 +0200

    Laravel - Mise en place des routes API

---
+++ appWebBDELaravel/routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\GoodieController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\ReservationGoodieController;
+use App\Http\Controllers\SoireeController;
+
+Route::get('/goodies', [GoodieController::class, 'index']);
+Route::get('/goodies/{id}', [GoodieController::class, 'show']);
+Route::post('/goodies', [GoodieController::class, 'store']);
+Route::put('/goodies/{id}', [GoodieController::class, 'update']);
+Route::delete('/goodies/{id}', [GoodieController::class, 'destroy']);
+
+Route::get('/reservations', [ReservationController::class, 'index']);
+Route::get('/reservations/{id}', [ReservationController::class, 'show']);
+Route::post('/reservations', [ReservationController::class, 'store']);
+Route::put('/reservations/{id}', [ReservationController::class, 'update']);
+Route::delete('/reservations/{id}', [ReservationController::class, 'destroy']);
+
+Route::get('/reservation-goodies', [ReservationGoodieController::class, 'index']);
+Route::get('/reservation-goodies/{id}', [ReservationGoodieController::class, 'show']);
+Route::post('/reservation-goodies', [ReservationGoodieController::class, 'store']);
+Route::put('/reservation-goodies/{id}', [ReservationGoodieController::class, 'update']);
+Route::delete('/reservation-goodies/{id}', [ReservationGoodieController::class, 'destroy']);
+
+Route::get('/soirees', [SoireeController::class, 'index']);
+Route::get('/soirees/{id}', [SoireeController::class, 'show']);
+Route::post('/soirees', [SoireeController::class, 'store']);
+Route::put('/soirees/{id}', [SoireeController::class, 'update']);
+Route::delete('/soirees/{id}', [SoireeController::class, 'destroy']);
