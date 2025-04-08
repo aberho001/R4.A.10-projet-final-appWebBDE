d176a57b16b9a7c08f1bf09debae5174554a41c0
Andoni Berho <aberho001@iutbayonne.univ-pau.fr>
2025-04-08 19:22:00 +0200

    Laravel : Réparation de l'API

--- appWebBDELaravel/database/migrations/2025_04_02_095804_create_reservations_table.php
+++ appWebBDELaravel/database/migrations/2025_04_02_095804_create_reservations_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('reservations', function (Blueprint $table) {
-            $table->id('id_reservation');
+            $table->id();
             $table->string('nom');
             $table->string(
                 'email',
