2a6f7ae9845d361ebbda5d6c886102f8d12d8b4b
Andoni Berho <aberho001@iutbayonne.univ-pau.fr>
2025-04-08 18:43:03 +0200

    Angular : Ajout du service reservation-goodies et connexion de celui-ci à l'API

--- appWebBDEAngular/src/app/reservation-goodies.service.ts
+++ appWebBDEAngular/src/app/reservation-goodies.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,26 @@ import { Injectable } from '@angular/core';
 })
 export class ReservationGoodiesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getReservationsGoodies() {
+    return this.http.get('http://localhost:8000/api/reservation_goodies');
+  }
+
+  getReservationGoodie(id: number) {
+    return this.http.get(`http://localhost:8000/api/reservation_goodies/${id}`);
+  }
+
+  createReservationGoodie(reservationGoodie: any) {
+    return this.http.post('http://localhost:8000/api/reservation_goodies', reservationGoodie);
+  }
+
+  updateReservationGoodie(id: number, reservationGoodie: any) {
+    return this.http.put(`http://localhost:8000/api/reservation_goodies/${id}`, reservationGoodie);
+  }
+
+  deleteReservationGoodie(id: number) {
+    return this.http.delete(`http://localhost:8000/api/reservation_goodies/${id}`);
+  }
+  
 }
