1650ee6baafd2f8b897aa35a27a24c34bc2a924f
Andoni Berho <aberho001@iutbayonne.univ-pau.fr>
2025-04-11 15:54:03 +0200

    Angular : vue soirée simple

--- appWebBDEAngular/src/app/app-routing.module.ts
+++ appWebBDEAngular/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListSoireeComponent } from './list-soiree/list-soiree.component';
 import { ListReservationComponent } from './list-reservation/list-reservation.component';
 import { ListGoodieComponent } from './list-goodie/list-goodie.component';
+import { SoireeComponent } from './soiree/soiree.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { GoodieComponent } from './goodie/goodie.component';
 
 
 const routes: Routes = [
   { path: 'soirees', component: ListSoireeComponent },
   { path: 'reservations', component: ListReservationComponent },
-  { path: 'goodies', component: ListGoodieComponent }
+  { path: 'goodies', component: ListGoodieComponent },
+  { path: 'soirees/:id', component: SoireeComponent },
+  { path: 'reservations/:id', component: ReservationComponent },
+  { path: 'goodies/:id', component: GoodieComponent }
 
 ];
 
--- appWebBDEAngular/src/app/soiree/soiree.component.ts
+++ appWebBDEAngular/src/app/soiree/soiree.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SoireeService } from '../soiree.service';
+import { Soiree } from '../soiree.model';
 
 @Component({
   selector: 'app-soiree',
   standalone: false,
   templateUrl: './soiree.component.html',
-  styleUrl: './soiree.component.scss'
+  styleUrls: ['./soiree.component.scss']
 })
-export class SoireeComponent {
+export class SoireeComponent implements OnInit {
+  soiree: Soiree | null = null;
+  errorMessage: string | null = null;
 
+  constructor(private soireeService: SoireeService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    
+    this.soireeService.getSoireeById(id).subscribe({
+      next: (data) => {
+        if (data) {
+          this.soiree = data;
+          this.errorMessage = null; // Réinitialiser l'erreur en cas de succès
+        } else {
+          this.soiree = null;
+          this.errorMessage = 'Cette soirée n\'existe pas.';
+        }
+      },
+      error: (err) => {
+        console.error('Erreur lors du chargement de la soirée :', err);
+        this.soiree = null;
+        this.errorMessage = 'Cette soirée n\'existe pas.';
+      }
+    });
+  }
 }
