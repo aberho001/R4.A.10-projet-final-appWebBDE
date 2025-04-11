4ff7edc4354782296dc0144029e4a430f88041d3
Andoni Berho <aberho001@iutbayonne.univ-pau.fr>
2025-04-11 17:47:44 +0200

    Angular : Suppression soiree okay

--- appWebBDEAngular/src/app/soiree/soiree.component.ts
+++ appWebBDEAngular/src/app/soiree/soiree.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SoireeService } from '../soiree.service';
 import { Soiree } from '../soiree.model';
 
@@ -13,7 +13,8 @@ export class SoireeComponent implements OnInit {
   soiree: Soiree | null = null;
   errorMessage: string | null = null;
 
-  constructor(private soireeService: SoireeService, private route: ActivatedRoute) {}
+  constructor(private soireeService: SoireeService, private route: ActivatedRoute, private router: Router,
+  ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -35,4 +36,35 @@ export class SoireeComponent implements OnInit {
       }
     });
   }
+  onEdit(): void {
+    if (this.soiree) {
+      this.router.navigate([`/soirees/edit/${this.soiree.id}`]);
+    } else {
+      console.error("La soirée est introuvable !");
+      alert("La soirée est introuvable.");
+    }
+  }
+  
+
+  onDelete(): void {
+    if (this.soiree) {
+      const confirmation = confirm('Êtes-vous sûr de vouloir supprimer cette soirée ?');
+      if (confirmation) {
+        this.soireeService.deleteSoiree(this.soiree.id).subscribe({
+          next: () => {
+            alert('Soirée supprimée avec succès !');
+            this.router.navigate(['/soirees']);
+          },
+          error: (err) => {
+            console.error('Erreur lors de la suppression :', err);
+            alert('Erreur lors de la suppression.');
+          }
+        });
+      }
+    } else {
+      console.error("La soirée est introuvable !");
+      alert("La soirée est introuvable.");
+    }
+  }
+  
 }
